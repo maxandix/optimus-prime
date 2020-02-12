dcc9219ad3f34900669980ab8dd1aff96b4a82bb
Бирюков Максим <maxandix@gmail.com>
2020-02-12 13:43:00 +0300

    added debugging print

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ def main():
     timestamp = None
     params = {}
     while True:
+        print('new iteration')
         try:
             if timestamp:
                 params = {'timestamp': timestamp}
